081c2a35dc40bd45c0d1688c323514e652ec43f6
Wolvenfang <tyrant.violence@gmail.com>
2021-08-29 17:02:38 -0400

    Initial Commit.

---
+++ TemporaryTablesEF/TemporaryTablesEF.API/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemporaryTablesEF.API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
