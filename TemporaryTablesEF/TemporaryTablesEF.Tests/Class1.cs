51d7b93b2ac427316288f56136a3385b1002f5c5
Wolvenfang <tyrant.violence@gmail.com>
2021-08-29 18:08:38 -0400

    2nd commit

--- TemporaryTablesEF/TemporaryTablesEF.Tests/Class1.cs
+++ TemporaryTablesEF/TemporaryTablesEF.Tests/Class1.cs
@@ -1,8 +1,18 @@
-﻿using System;
+﻿using Bogus;
+using System;
 
 namespace TemporaryTablesEF.Tests
 {
+    public class Test
+    {
+        public string Name { get; set; }
+    }
     public class Class1
     {
+        public void Test() 
+        {
+
+        }
+        
     }
 }
